04167612f930aabcafec997e91506eb03326f4fe
sabes2024 <sabes.web@gmail.com>
2025-08-16 20:04:13 -0500

    subida de archivos --version 1.0

---
+++ main.py
@@ -0,0 +1,98 @@
+import time
+
+
+def obtener_precio():
+    try:
+        precio_venta_base = int(input("Ingrese el precio de venta base: "))
+        return precio_venta_base
+    except ValueError:
+        print("\n\n❌ Error, ingrese un número entero válido. No se permiten otros caracteres")
+        print("Ejemplo válido: 250000, 10000, 2500, 500\n")
+        return obtener_precio()
+
+
+def calcular_precio_venta():
+    precio_venta_base = obtener_precio()
+    precio_con_iva = precio_venta_base * 1.19
+    print(f"\n\n💰--> Con IVA del 19% lo venderías en: ${precio_con_iva:,.0f} COP")
+    print("=================================================")
+
+    time.sleep(1.3)
+    calcular_precio_con_comision(precio_con_iva)
+
+
+
+def calcular_precio_con_comision(precio_venta_con_iva):
+
+    print("\nTe van a pagar por datáfono?")
+    opcion_datafono = input("    1. Sí, comisión del 3%    \n    2. Si, comisión del 3,5%\n    3. No\n\n\t--> :  ")
+    if opcion_datafono == "1":
+        porcentaje_comision = 3
+        precio_con_comision = precio_venta_con_iva * 1.03
+
+    elif opcion_datafono == "2":
+        porcentaje_comision = 3.5
+        precio_con_comision = precio_venta_con_iva * 1.035
+
+    elif opcion_datafono == "3":
+        time.sleep(1.3)
+        calcular_precio_con_cuatropormil(precio_venta_con_iva, False)
+        return
+    else:
+        print("\nOpción no válida, por favor ingresa 1,2 o 3.\n")
+        time.sleep(1)
+        return calcular_precio_con_comision(precio_venta_con_iva)
+    
+    print(f"\n\n💳--> Con IVA del 19% + comisión {porcentaje_comision}%: ${precio_con_comision:,.0f} COP")
+    print("=================================================")
+    time.sleep(1.3)
+    calcular_precio_con_cuatropormil(precio_con_comision, True)
+
+
+
+def calcular_precio_con_cuatropormil(precio, tiene_comision):
+
+    respuesta = input("\n\t¿Desea agregar el 4x1000 a esta venta?\n\t    1. Sí\n\t    2. No\n\n\t\t--> :  ")
+    if respuesta == "1":
+        precio_con_cuatropormil = precio * 1.004
+
+        if tiene_comision:
+            print(f"\n\n🏦--> Con IVA del 19% + comisión + 4x1000: ${precio_con_cuatropormil:,.0f} COP")
+        else:
+            print(f"\n\n🏦--> Con IVA del 19% + 4x1000: ${precio_con_cuatropormil:,.0f} COP")
+        print("=================================================\n")
+    elif respuesta == "2":
+        return
+    else:
+        print("\nOpción no válida, por favor ingresa 1 o 2.\n")
+        time.sleep(1)
+        return calcular_precio_con_cuatropormil(precio)
+
+
+
+def otra_consulta():
+    respuesta = input("¿Deseas hacer otra consulta?\n1. Sí\n2. No\n\n\t--> :  ")
+    if respuesta == "1":
+        return True
+    elif respuesta == "2":
+        return False
+    else:
+        print("\nOpción no válida, por favor ingresa 1 o 2.\n")
+        time.sleep(1)
+        return otra_consulta()
+
+
+################# MENU INTERACTIVO
+
+print("¿No sabes cuánto cobrar por tu producto? calcula el precio de venta exacto, incluyendo todos sus impuestos")
+time.sleep(2)
+print("\n\nCALCULADORA DE PRECIO DE VENTA")
+print("=================================================\n")
+
+while True:
+    calcular_precio_venta()
+    time.sleep(1)
+    
+    if not otra_consulta():
+        print("\n\nGracias por usar la calculadora de precio de venta...\n\n")
+        break 
---
+++ readme.md
@@ -0,0 +1,19 @@
+# 💰 Calculadora de Precio de Venta (Colombia)
+
+Una herramienta sencilla y precisa para ayudarte a determinar el precio de venta ideal de tus productos y servicios en Colombia, asegurando que todos los costos e impuestos estén cubiertos y que tu rentabilidad sea la correcta.
+
+---
+
+## ***✨ ¿Qué hace por ti?***
+
+* ### **Pone el IVA (19%)**
+  Se encarga de agregar ese impuesto clave sin que tengas que sacar la calculadora.
+
+* ### **Añade la comisión del datáfono**
+  Te ayuda a considerar si el cliente te pagará con tarjeta (3% o 3.5% de comisión) para que no pierdas ni un peso de tu ganancia.
+
+* ### **Tiene en cuenta el 4x1000**
+  Ese pequeño costo invisible ya está incluido en el cálculo para darte un precio final mucho más realista.
+
+* ### **Es fácil de usar**
+  Sólo sigue las instrucciones en pantalla.
